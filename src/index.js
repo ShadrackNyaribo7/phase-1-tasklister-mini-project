8379e51f323ae47ffe496c61db1bf4f174294fd2
shadracknyaribo <stilespulsar77@gmail.com>
2022-11-28 14:13:36 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+  const taskDescription = document.querySelector("#new-task-description");
+
+  taskDescription.addEventListener("click", checkboxClick, false);
+
+
+  let warn = "preventDefault() this shall not pass!<br>";
+  document.getElementById("new-task-description").innerHTML += warn;
+  event.preventDefault();
 });
